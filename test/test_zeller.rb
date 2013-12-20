066b01d9a5bc765a48651a9a7447c784c34db3ab
Lars Söderkvist <updates11@mac.com>
2013-12-19 23:48:52 -0600

    Add test for returning correct day of the week

--- test/test_zeller.rb
+++ test/test_zeller.rb
@@ -7,4 +7,9 @@ class TestZeller < MiniTest::Unit::TestCase
     Zeller.calculate("April 16, 1896")
   end
 
+  def test_zeller_returns_correct_day_of_the_week
+    z = Zeller.calculate("March 1, 2012")
+    assert_equal z, 5
+  end
+
 end
