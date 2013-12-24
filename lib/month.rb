2a83d42fd57eebc0c1fbbb32990adfb0d250dd4e
Lars Söderkvist <updates11@mac.com>
2013-12-23 23:45:23 -0700

    Write firstdayzeller method that passes test_month.rb tests

--- lib/month.rb
+++ lib/month.rb
@@ -4,5 +4,17 @@ class Month
   end
 
   def firstdayzeller date_array
+    year = date_array[0]
+    month = date_array[1]
+    raise ArgumentError, 'Year must be within the range of 1800–3000' if year < 1800 || year > 3000
+    if month == 1
+      month = 13
+      year = year - 1
+    elsif month == 2
+      month = 14
+      year = year - 1
+    end
+    dayofweek = (1 + (26 * (month + 1)/10) + year + (year/4) + (6 * (year/100)) + (year/400)).modulo(7)
   end
+
 end
