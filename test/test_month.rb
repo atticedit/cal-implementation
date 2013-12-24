95188328815257f12ce28b21469443268e21f683
Lars Söderkvist <updates11@mac.com>
2013-12-23 23:34:24 -0700

    Change argument passed to zeller from integer to array

--- test/test_month.rb
+++ test/test_month.rb
@@ -4,23 +4,23 @@ require 'minitest/autorun'
 class TestMonth < MiniTest::Unit::TestCase
 
   def test_zeller_returns_correct_day_of_week_for_leap_year_January_1st
-    assert_equal 1, (Month.new.zeller 20120101)
+    assert_equal 1, (Month.new.zeller [2012,01])
   end
 
   def test_zeller_returns_correct_day_of_week_for_leap_year_February_1st
-    assert_equal 4, (Month.new.zeller 20120201)
+    assert_equal 4, (Month.new.zeller [2012,02])
   end
 
   def test_zeller_returns_correct_day_of_week_for_leap_year_March_1st
-    assert_equal 5, (Month.new.zeller 20120301)
+    assert_equal 5, (Month.new.zeller [2012,03])
   end
 
   def test_zeller_returns_correct_day_of_week_for_common_year_March_1st
-    assert_equal 3, (Month.new.zeller 20110301)
+    assert_equal 3, (Month.new.zeller [2011,03])
   end
 
   def test_zeller_returns_correct_day_of_week_for_hundred_divisible_common_year_March_1st
-    assert_equal 5, (Month.new.zeller 19000301)
+    assert_equal 5, (Month.new.zeller [1900,03])
   end
 
   def test_zeller_throws_error_for_blank_input
@@ -30,14 +30,14 @@ class TestMonth < MiniTest::Unit::TestCase
   end
 
   def test_zeller_throws_error_for_incorrect_format
-    assert_raises ArgumentError do
-      Month.new.zeller 2000
+    assert_raises NoMethodError do
+      Month.new.zeller [2000]
     end
   end
 
   def test_zeller_throws_error_for_year_outside_scope
     assert_raises ArgumentError do
-      Month.new.zeller 15000301
+      Month.new.zeller [1500,03]
     end
   end
 
