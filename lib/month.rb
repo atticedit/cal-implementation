d0f051891e1468a9afa2697e04e9186b9c9c1ba0
Lars Söderkvist <updates11@mac.com>
2014-01-03 00:04:27 -0600

    Refactor month.rb to be more idiomatic

--- lib/month.rb
+++ lib/month.rb
@@ -6,20 +6,14 @@ class Month
 
     raise ArgumentError, 'Year must be within the range of 1800–3000' if year < 1800 || year > 3000
 
-    if month == 1
-      month = 13
-      year = year - 1
-    elsif month == 2
-      month = 14
-      year = year - 1
+    case month
+    when 1 then month = 13 and year -= 1
+    when 2 then month = 14 and year -= 1
     end
 
     dayofweek = (1 + (26 * (month + 1)/10) + year + (year/4) + (6 * (year/100)) + (year/400)).modulo(7)
-
-    if dayofweek == 0
-      dayofweek = 7
-    else dayofweek
-    end
+    dayofweek += 7 if dayofweek == 0
+    dayofweek
 
   end
 
